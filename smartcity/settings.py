7cc46269f8f973c2a1bf3b495856269c41f3a673
mohammed <khamedkh297@gmail.com>
2024-12-19 23:12:36 +0100

    update settings

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os 
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
